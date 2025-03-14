468aec55106e7b46e0992f6285c2fc1bd92ab404
Rik Visser <rik@techtalks.network>
2025-03-14 01:01:43 +0100

    Update docker run command flag for Liquidsoap configuration

--- installer.sh
+++ installer.sh
@@ -340,7 +340,7 @@ docker run -d \
     -v ${CONFIG_DIR}/$CONFIGNAME.liq:/scripts/current.liq \
     -v ${CONFIG_DIR}/$CONFIGNAME.wav:/audio/fallback.wav \
     -e TZ=$TIMEZONE \
-    -c /scripts/current.liq \
+    --command /scripts/current.liq \
     --restart unless-stopped \
     --name ${CONFIGNAME}_liquidsoap \
     pltnk/liquidsoap
